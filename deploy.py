222627350a63f1d897ebdc5a6fd7b1c957a09fbc
Ryan Wellborn <ryan.wellborn@protonmail.com>
2020-06-29 22:50:29 -0600

    Moved Sagemaker model serving functions from train to deploy file.

---
+++ deploy.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from .train import argparse
+
+
+def model_fn(
+    model_dir,
+    stack_sizes,
+    R_stack_sizes,
+    A_filter_sizes,
+    Ahat_filter_sizes,
+    R_filter_sizes,
+    data_format,
+):
+    """load model function for SageMaker. Must pass in model 
+    parameters as kwargs when calling deploy."""
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = PredNet(
+        stack_sizes,
+        R_stack_sizes,
+        A_filter_sizes,
+        Ahat_filter_sizes,
+        R_filter_sizes,
+        output_mode="prediction",
+        data_format=data_format,
+    )
+    with open(os.path.join(model_dir, "model.pth"), "rb") as f:
+        model.load_state_dict(torch.load(f))
+    return model.to(device)
+
+
+# def input_fn(request_body, request_content_type):
+#     pass
+
+
+# def predict_fn(input_object, model):
+#     pass
+
+
+# def output_fn(prediction, response_content_type):
+#     pass
