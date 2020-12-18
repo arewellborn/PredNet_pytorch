243159fc415d56df6ce27eb5f76f6c97ee2593c5
EC2 Default User <ec2-user@ip-172-16-88-223.us-west-2.compute.internal>
2020-12-18 04:25:21 +0000

    Corrections on tar.gz.

--- train_dni.py
+++ train_dni.py
@@ -7,6 +7,7 @@ import time
 from datetime import datetime
 import numpy as np
 import json
+import tarfile
 
 import torch
 from torch.autograd import Variable
@@ -299,6 +300,16 @@ if __name__ == "__main__":
             output_mode="prediction",
             data_format=data_format,
         )
+        if '.pth' in load_model:
+            load_model = load_model
+        elif '.tar.gz' in load_model:
+            tar = tarfile.open(load_model, 'r:gz')
+            outpath = load_model.rsplit('/', 1)[0]
+            tar.extractall(path=outpath)
+            tar.close()
+            load_model = os.path.join(outpath, 'model.pth')
+        else:
+            raise RuntimeError
         prednet.load_state_dict(torch.load(load_model))
         prednet.train()
         print("Existing model successsfully lodaded.")
